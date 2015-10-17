5e8cceecb148171f5656741d13d29eaed8972106
FlancrestEnterprises <josiah.liesemer@gmail.com>
2015-10-17 03:45:30 -0400

    lab 5

---
+++ ExercisesViewModels/DepartmentViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using ExercisesDAL;
+namespace ExercisesViewModels
+{
+   public class DepartmentViewModel
+    {
+        private DepartmentDAO _dao;
+
+        public string DepartmentId { get; set; }
+
+        public string DepartmentName { get; set; }
+
+        //constructor
+        public DepartmentViewModel()
+        {
+
+            _dao = new DepartmentDAO();
+
+        }
+
+        //find dept from dal
+        public void GetById()
+        {
+
+            try
+            {
+                Department dept = _dao.GetById(DepartmentId);
+
+                DepartmentId = dept._id.ToString();
+                DepartmentName = dept.DepartmentName;
+            }
+            catch (Exception ex)
+            {
+                DepartmentId = "not found";
+            }
+
+
+        }
+
+
+    }
+
+
+}
